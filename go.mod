657942581a519a51282378e033b579c077712b0b
BTabaku <baftit40@gmail.com>
2024-10-30 11:42:17 +0100

    Update go.mod and go.sum files to include github.com/joho/godotenv v1.5.1 as an indirect requirement

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module generic-api-sync-service
 go 1.22.5
 
 require gopkg.in/yaml.v2 v2.4.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,12 @@
 package config
 
 import (
-	// "log"
+	"bytes"
+	"log"
 	"os"
+	"text/template"
 
+	"github.com/joho/godotenv"
 	"gopkg.in/yaml.v2"
 )
 
@@ -22,14 +25,43 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	file, err := os.Open("config/config.yaml")
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	// Read the YAML file content
+	file, err := os.ReadFile("config/config.yaml")
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
-	config := &Config{}
-	if err := yaml.NewDecoder(file).Decode(config); err != nil {
+
+	// Replace environment variable placeholders
+	tmpl, err := template.New("config").Parse(string(file))
+	if err != nil {
 		return nil, err
 	}
+
+	var resolvedConfig bytes.Buffer
+	if err := tmpl.Execute(&resolvedConfig, getEnvMap()); err != nil {
+		return nil, err
+	}
+
+	// Unmarshal the modified YAML content into the Config struct
+	config := &Config{}
+	if err := yaml.NewDecoder(&resolvedConfig).Decode(config); err != nil {
+		return nil, err
+	}
+
 	return config, nil
+}
+
+// getEnvMap returns a map of environment variables
+func getEnvMap() map[string]string {
+	envMap := make(map[string]string)
+	for _, env := range os.Environ() {
+		pair := bytes.SplitN([]byte(env), []byte{'='}, 2)
+		envMap[string(pair[0])] = string(pair[1])
+	}
+	return envMap
 }
